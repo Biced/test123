704142af17b9fbc8453467e805e4cafa095cea15
Biced <biced.nff@gmail.com>
2020-06-13 23:39:11 +0300

    trying to deploy woth db

--- server.js
+++ server.js
@@ -26,35 +26,14 @@ app.use('/api/product', productRoute);
 app.use('/api/cart', cartRoute);
 // app.use('/api/category', categoryRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server listen from ${PORT}.....`);
-});
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:admin1234@ds141674.mlab.com:41674/heroku_pgrhkdk1', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_pgrhkdk1:1cdsb8un9kvhb93jg8ortjmbug@ds141674.mlab.com:41674/heroku_pgrhkdk1', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!!!!');
-});
 
-// Data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const path = require('path');
 
-// Step 3
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-// }
-
-// app.listen(PORT, console.log(`Server is starting at ${PORT}`));
-
-// just a deploy test
-// const express = require('express');
-// const path = require('path');
-// const app = express();
 
 app.use(express.static(__dirname + '/dist/myStoreApp'));
 app.get('/*', function (req, res) {
@@ -62,4 +41,4 @@ app.get('/*', function (req, res) {
         '/dist/myStoreApp/index.html'));
 });
 
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
